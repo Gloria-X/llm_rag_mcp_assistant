6fe5d54494e187f4c938a06d245f5ef8d5dea1ea
Gloria-X <656369946@qq.com>
2025-04-23 17:37:56 +0800

    feat: rerank

--- src/Agent.ts
+++ src/Agent.ts
@@ -1,23 +1,28 @@
 import ChatOpenAI from "./ChatOpenAI";
 import MCPClient from "./MCPClient";
 import { log } from "./utils";
+import { Embedder } from "./Embedding";
+import { createDBs } from "./models";
+import { Reranker } from "./Rerank";
 
 export default class Agent {
     private mcpClients: MCPClient[];
-    private llm: ChatOpenAI | null = null
-    private model: string
-    private systemPrompt: string
-    private context: string
+    private llm: ChatOpenAI | null = null;
+    private model: string;
+    private systemPrompt: string;
+    private embedder: Embedder;
+    private dbs: ReturnType<typeof createDBs>;
+
     constructor(
         model: string,
         mcpClients: MCPClient[],
         systemPrompt: string = '',
-        context: string = ''
     ) {
         this.mcpClients = mcpClients;
         this.model = model;
         this.systemPrompt = systemPrompt;
-        this.context = context;
+        this.embedder = new Embedder();
+        this.dbs = createDBs();
     }
 
     public async init() {
@@ -37,11 +42,89 @@ export default class Agent {
         }
     }
 
-    async invoke(prompt: string) {
-        if (!this.llm) throw new Error('LLM not initialized')
-        let response = await this.llm.chat(prompt);
+    // similarity 余弦相似度
+    private cosSimilarity(a: number[], b: number[]): number {
+        const dotProduct = a.reduce((sum, val, i) => sum + val * b[i], 0);
+        const normA = Math.sqrt(a.reduce((sum, val) => sum + val * val, 0));
+        const normB = Math.sqrt(b.reduce((sum, val) => sum + val * val, 0));
+        return dotProduct / (normA * normB);
+    }
+    
+    private async findSimilarSegments(query: string, topK: number = 3): Promise<{
+        segments: { content: string; documentId: number }[];
+        documentIds: number[];
+    }> {
+        try {
+            // embedding query
+            const queryEmbedding = await this.embedder.createEmbedding(query);
+            // const queryVector = `[${queryEmbedding.join(',')}]`;
+
+            const allEmbeddings = await this.dbs.embeddingDB.findAll();
+
+            // sort
+            const similarities = [] as {
+                similarity: number;
+                segmentId: number;
+            }[]
+            for (const emb of allEmbeddings) {
+                const similarity = this.cosSimilarity(queryEmbedding, JSON.parse(emb.embedding))
+                const similarityRes = {
+                    similarity,
+                    segmentId: emb.document_segment_id
+                }
+                similarities.push(similarityRes)
+            }
+            similarities.sort((a, b) => b.similarity - a.similarity);
+
+            // top_k
+            let topSegments: { content: string; documentId: number }[] = [];
+            const topKSimilarities = similarities.slice(0, topK);
+            for (const sim of topKSimilarities) {
+                const segment = await this.dbs.documentSegmentDB.findById(sim.segmentId)
+                if (segment) {
+                    // log(`FIND SEGMENT - ${sim.segmentId} - FROM DOCUMENT - ${segment.document_id}`)
+                    // console.log(`segment: ${segment.content}`)
+
+                    topSegments.push({
+                        content: segment.content,
+                        documentId: segment.document_id
+                    });
+                }
+            }
+
+            log('FIND SIMILAR SEGMENTS')
+
+            const documentIds = [...new Set(topSegments.map(s => s.documentId))];
+
+            return {
+                segments: topSegments,
+                documentIds
+            };
+        } catch (error) {
+            console.error('findSimilarSegments err:', error);
+            throw error;
+        }
+    }
+
+    async invoke(prompt: string, topk: number = 3) {
+        if (!this.llm) throw new Error('LLM not initialized');
+    
+        // RAG
+        const { segments, documentIds } = await this.findSimilarSegments(prompt, topk * 2);
+    
+        // Rerank
+        const reranker = new Reranker();
+        const rerankedSegments = await reranker.rerank(prompt, segments, topk);
+
+        const context = rerankedSegments.map(s => s.content).join('\n\n');
+        const enhancedPrompt = `context: \n${context}\n\nuser prompt: ${prompt}`;
+
+        log('GENERATE CONTEXT SUCCESS')
+
+        let response = await this.llm.chat(enhancedPrompt);
+        
         while (true) {
-            // 处理 toolCalls
+            // tools
             if (response.toolCalls.length > 0) {
                 for (const toolCall of response.toolCalls) {
                     const mcp = this.mcpClients.find(mcp => mcp.getTools().find(t => t.name === toolCall.function.name));
@@ -49,7 +132,7 @@ export default class Agent {
                         this.llm.appendToolResult(toolCall.id, 'Error: Tool not found');
                     } else {
                         log(`TOOL USE - ${toolCall.function.name}`)
-                        console.log(`Calling Toll: ${toolCall.function.name} with ${toolCall.function.arguments}`)
+                        console.log(`calling toll: ${toolCall.function.name} with ${toolCall.function.arguments}`)
                         const result = await mcp.callTool(toolCall.function.name, JSON.parse(toolCall.function.arguments));
                         // console.log(`TOOL RESULT - ${JSON.stringify(result)}`)
                         this.llm.appendToolResult(toolCall.id, JSON.stringify(result));
@@ -58,8 +141,13 @@ export default class Agent {
                 response = await this.llm.chat();
                 continue;
             }
-            await this.close();
-            return response.content
+            break
         }
+        await this.close();
+
+        return {
+            content: response.content,
+            documentIds
+        };
     }
 }
---
+++ src/Rerank.ts
@@ -0,0 +1,70 @@
+import { log } from './utils'
+
+interface RerankResponse {
+    id: string;
+    results: {
+        document: {
+            text: string;
+        };
+        index: number;
+        relevance_score: number;
+    }[];
+    tokens: {
+        input_tokens: number;
+        output_tokens: number;
+    };
+}
+
+export class Reranker {
+    private readonly apiUrl: string = process.env.RERANK_BASE_URL!;
+    private readonly token: string = process.env.RERANK_KEY!;
+    private readonly model: string = process.env.RERANK_MODEL!;
+
+    constructor() {}
+    async rerank(query: string, segments: { content: string; documentId: number }[], topK: number): Promise<{ content: string; documentId: number }[]> {
+        const documents = segments.map(s => s.content);
+        
+        // https://docs.siliconflow.cn/cn/api-reference/rerank/create-rerank
+        const options = {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${this.token}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                model: this.model,
+                query,
+                documents,
+                top_n: topK,
+                return_documents: false,
+                max_chunks_per_doc: 1024,
+                overlap_tokens: 80
+              })
+        };
+
+        try {
+            const response = await fetch(this.apiUrl, options);
+            if (!response.ok) {
+                throw new Error(`rerank - API request err: ${response.statusText}`);
+            }
+            const result: RerankResponse = await response.json();
+
+            console.log('rerank response:', JSON.stringify(result));
+
+            const rerankedSegments = result.results
+                .sort((a, b) => b.relevance_score - a.relevance_score)
+                .map(result => ({
+                    content: segments[result.index].content,
+                    documentId: segments[result.index].documentId
+                }))
+                .slice(0, topK);
+            
+            log('RERANK SUCCESS')
+
+            return rerankedSegments;
+        } catch (error) {
+            console.error('rerank err:', error);
+            throw error;
+        }
+    }
+}
